7b55b6ba77056deda1bad234de4f65398ef39bf4
Nicolas <n.serna@utp.edu.co>
2023-03-25 16:32:27 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Punto2T2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public static List<Producto> productos = new List<Producto>()
+        {
+            new Producto
+            {
+                nombre = "jabon",
+                codigo = "A21",
+                precio = 1500.50,
+                estado = true
+            },
+            new Producto
+            {
+                nombre = "jabon",
+                codigo = "A21",
+                precio = 1500.50,
+                estado = true
+            }
+        };
+        [HttpGet("Consultar")]
+        public List<Producto> Consultar()
+        {
+            return productos;
+        }
+
+
+        [HttpGet("Consultar 2")]
+        public string Consultar2()
+        {
+            return "Hola Mundo 2";
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace Punto2T2
+{
+    public class Producto
+    {
+        public string nombre { get; set; }
+        public string codigo { get; set; }
+        public double precio { get; set;}
+        public bool estado { get; set;}
+
+    }
+}
